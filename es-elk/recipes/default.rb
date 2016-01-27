a5ca16e8fdf3723f6582b71c9bccae682df6e345
Maz <mazharul2007@gmail.com>
2016-01-27 19:22:07 +0800

    modifying values

--- es-elk/recipes/default.rb
+++ es-elk/recipes/default.rb
@@ -18,19 +18,20 @@ end
 # 
 
 elasticsearch_configure 'elasticsearch' do
-	allocated_memory '2G'
+	allocated_memory '512m'
 	configuration({
 		'cluster.name' => 'universe',
   	'discovery.type' => 'ec2',
-  	'discovery.ec2.groups' => 'es-elasticsearch',
+  	'discovery.ec2.groups' => 'es-cluster',
   	'discovery.ec2.availability_zones' => 'ap-southeast-1a',
   	'cloud.aws.region' => 'ap-southeast-1',
-    'discovery.ec2.tag.stage' => 'sandbox',
   	'discovery.zen.ping.multicast.enabled' => 'false'
 	})
 end
 
-elasticsearch_service 'elasticsearch'
+elasticsearch_service 'elasticsearch' do
+  service_actions [:enable, :start]
+end
 
 
 # install plugin
@@ -38,9 +39,11 @@ elasticsearch_service 'elasticsearch'
 
 elasticsearch_plugin 'cloud-aws' do
 	action :install
+  notifies :restart, 'elasticsearch_service[elasticsearch]', :delayed
 end
 
 elasticsearch_plugin 'kopf' do
   url 'lmenezes/elasticsearch-kopf'
   action :install
+  notifies :restart, 'elasticsearch_service[elasticsearch]', :delayed
 end
