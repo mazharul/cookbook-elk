4d02335afafa1e0214d37cbdcce2d201f0a974d4
unknown <Mazharul@MIS-11.jobst.jobstreet.com>
2016-02-11 17:28:30 +0800

    Installing default logstash version

--- es-elk/attributes/default.rb
+++ es-elk/attributes/default.rb
@@ -4,3 +4,7 @@ default['kibana']['webserver'] = '' # nginx or apache
 default['kibana']['install_type'] = 'git'
 default['kibana']['git']['url'] = 'https://github.com/elastic/kibana'
 default['kibana']['git']['branch'] = 'v4.3.1'
+
+
+# We will install updated version of logstash
+default['logstash']['instance_default']['version']        = '2.1.2'
